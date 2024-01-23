62f11de24254b664854b1c324f819b5e42fcf6a6
faiq28 <faiqafifalthf@gmail.com>
2024-01-23 11:01:12 +0700

    m

--- src/app/kategori/kategori.service.ts
+++ src/app/kategori/kategori.service.ts
@@ -8,14 +8,10 @@ import {
 import { REQUEST } from '@nestjs/core';
 import { InjectRepository } from '@nestjs/typeorm';
 import BaseResponse from 'src/utils/response/base.response';
-import { Like, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Kategori } from './kategori.entity';
-import {
-  CreateKategoriDto,
-  UpdateKategoriDto,
-  findAllKategori,
-} from './kategori.dto';
-import { ResponsePagination, ResponseSuccess } from 'src/interface';
+import { CreateKategoriDto, UpdateKategoriDto } from './kategori.dto';
+import { ResponseSuccess } from 'src/interface';
 
 @Injectable()
 export class KategoriService extends BaseResponse {
@@ -93,38 +89,5 @@ export class KategoriService extends BaseResponse {
       message: 'Berhasil Menghapus kategori',
       data: hapus,
     };
-  }
-
-  async getAllCategory(query: findAllKategori): Promise<ResponsePagination> {
-    const { page, pageSize, limit, nama_kategori } = query;
-
-    const filterQuery = {};
-    if (nama_kategori) {
-      filterQuery.nama_kategori = Like(`%${nama_kategori}%`);
-    }
-    const total = await this.kategoriRepository.count({
-      where: filterQuery,
-    });
-    const result = await this.kategoriRepository.find({
-      where: filterQuery,
-      relations: ['created_by', 'updated_by'], // relasi yang aka ditampilkan saat menampilkan list kategori
-      select: {
-        // pilih data mana saja yang akan ditampilkan dari tabel kategori
-        id: true,
-        nama_kategori: true,
-        created_by: {
-          id: true, // pilih field  yang akan ditampilkan dari tabel user
-          nama: true,
-        },
-        updated_by: {
-          id: true, // pilih field yang akan ditampilkan dari tabel user
-          nama: true,
-        },
-      },
-      skip: limit,
-      take: pageSize,
-    });
-
-    return this._pagination('OK', result, total, page, pageSize);
   }
 }
